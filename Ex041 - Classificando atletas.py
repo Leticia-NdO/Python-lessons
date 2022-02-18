a9cf36c494d79c3df8b38379a66e9a86c1c6c60e
Leticia <leticianevesdeoliveira1@gmail.com>
2022-02-18 11:17:24 -0300

    Feitoria do exercício classificando atletas

--- Ex041 - Classificando atletas.py
+++ Ex041 - Classificando atletas.py
@@ -1,1 +1,16 @@
-texto = str(input('Digite o seu poema aqui: '))
+from datetime import date
+nas = int(input('Ano de nascimento: '))
+idade = date.today().year - nas
+print('O atleta tem {} anos'.format(idade))
+if idade <= 9:
+    print('Classificação: MIRIM')
+elif idade <= 14:
+    print('Classificação: INFANTIL')
+elif idade <= 19:
+    print('Classificação: JÚNIOR')
+elif idade <= 25:
+    print('Classificação: SÊNIOR')
+elif idade >= 25:
+    print('Classificação: MASTER')
+else:
+    print('Por favor, insira uma idade válida.')
