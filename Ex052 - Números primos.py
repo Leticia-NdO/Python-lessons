492a2db925984228c5467adad0e31637fc3d821b
Leticia <leticianevesdeoliveira1@gmail.com>
2022-02-22 12:50:52 -0300

    Finalização do exercício de números primos com cores no terminal

--- Ex052 - Números primos.py
+++ Ex052 - Números primos.py
@@ -1,11 +1,13 @@
 count = 0
 n = int(input('Digite um número: '))  # o programa deve contar de 10 até n e dizer se o número é primo ou não.
-for c in range(1, n + 1):  #
-    print(c, end=' ')
-    if n % c == 0:
+for c in range(1, n + 1):  # se eu quiser printar c, o último número não aparece, então pra n aparecer tenho que colocar
+    if n % c == 0:         # um a mais.
+        print('\033[31m', end='')  # se n for divisível por c, colore de vermelho e conta ele.
         count += 1
+    else:
+        print('\033[m', end='')  # se n não for divisível por c, limpa a coloração e não conta ele.
+    print(c, end=' ')
 if count == 2:
-    print(f'\nO número {n} foi divisível 2 vezes\nE por isso ele é PRIMO!')
+    print(f'\033[m\nO número {n} foi divisível 2 vezes\nE por isso ele é PRIMO!')
 else:
-    print(f'\nO número {n} foi divisível {count} vezes\nE por isso ele NÃO É PRIMO!')
-\033[m
+    print(f'\033[m\nO número {n} foi divisível {count} vezes\nE por isso ele NÃO É PRIMO!')
